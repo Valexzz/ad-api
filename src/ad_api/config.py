dfcb7bbb15dda5e254490d58910eff7a91d25928
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-09-20 23:33:06 -0300

    chore: adiciona ad para testes

--- src/ad_api/config.py
+++ src/ad_api/config.py
@@ -4,9 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     servidor_ad: str
     dominio_ad: str
+    domain_ad: str
     usuario_service_account_ad: str
     senha_usuario_service_account_ad: str
-    dn_base_ad  : str
+    dn_base_ad: str
     timeout_ldap: int = 5
 
     model_config = SettingsConfigDict(env_file=".env")
